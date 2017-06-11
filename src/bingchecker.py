199336bc8f7512e1b42d8f0dcd9d67f644bfab3d
ggusoft <info@ggusoft.com>
2017-06-11 22:15:45 +0200

    Updating robtex

--- src/bingchecker.py
+++ src/bingchecker.py
@@ -62,7 +62,7 @@ class DomainSearch:
         resul = []
         try:
             r = requests.get(url, headers=headers, verify=False)
-            ol = r.text.split("class=\"xbul\"")
+            ol = r.text.split("class=\"xbul")
             li = ol[1].split("<li>")
             li[len(li) - 17] = li[len(li) - 17].split("</ol>")[0]
             for a in range(1, len(li) - 16):
